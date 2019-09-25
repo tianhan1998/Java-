637b7d2d325e3bacc9d86a722f0514056ed3bb55
tianhan <892265525@qq.com>
2019-09-25 17:29:27 +0800

    加减乘除带小数点

--- calculator/src/cn/itcast/jcalculator/JCalculator.java
+++ calculator/src/cn/itcast/jcalculator/JCalculator.java
@@ -2,6 +2,9 @@ package cn.itcast.jcalculator;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
 import javax.swing.*;
  
 public class JCalculator extends JFrame implements ActionListener {
@@ -15,9 +18,22 @@ public class JCalculator extends JFrame implements ActionListener {
            System.exit(0);
        }
     }
-	double left=0;
-	double right=0;
+	
+	BigInteger bileft;
+	BigInteger biright;
+	BigDecimal bdleft;
+	BigDecimal bdright;
+	
+//	double dleft=0;
+//	double dright=0;
+//
+//	long lleft=0;
+//	long lright=0;
+
+//	boolean isbig=false;
+
 	char op;
+	boolean isdouble=false;
     int i;
     // Strings for Digit & Operator buttons.
     private final String[] str = { "7", "8", "9", "/", "4", "5", "6", "*", "1",
@@ -64,63 +80,150 @@ public class JCalculator extends JFrame implements ActionListener {
        // The "close" button "X".
        addWindowListener(new WindowCloser());
        // Initialize the window size.
-        setSize(1024, 768);
+        setSize(640, 480);
        // Show the window.
        // show(); Using show() while JDK version is below 1.5.
        setVisible(true);
        // Fit the certain size.
     }
-   
+//    public boolean isLong(String number){
+//    	try{
+//			long test=Long.parseLong(number);
+//			return true;
+//    	}catch(NumberFormatException e){
+//    		return false;
+//    	}
+//    }
+//    public boolean isDouble(String number){
+//    	try{
+//    		double test=Double.parseDouble(number);
+//    		return true;
+//    	}catch(NumberFormatException e){
+//    		return false;
+//    	}
+//    }
     public void actionPerformed(ActionEvent e) {
        Object target = e.getSource();
        String label = e.getActionCommand();
        if (target == reset)
            handleReset();
-       else if ("0123456789.".indexOf(label) > 0)
+       else if ("0123456789.".indexOf(label) >= 0)
            handleNumber(label);
        else
            handleOperator(label);
     }
+    public void calculateBigInteger(BigInteger left,BigInteger right,char op){
+		switch(op){
+		case '+':display.setText(left.add(right).toString());break;
+		case '-':display.setText(left.subtract(right).toString());break;
+		case '*':display.setText(left.multiply(right).toString());break;
+		case '/':display.setText(left.divide(right).toString());break;
+		}
+    }
+    public void calculateBigDecimal(BigDecimal left,BigDecimal right,char op){
+		switch(op){
+		case '+':display.setText(left.add(right).toString());break;
+		case '-':display.setText(left.subtract(right).toString());break;
+		case '*':display.setText(left.multiply(right).toString());break;
+		case '/':display.setText(left.divide(right).toString());break;
+		}
+    }
+//    public long calculateLong(long left,long right,char op){
+//    	long answer=0;
+//		switch(op){
+//		case '+':answer=left+right;break;
+//		case '-':answer=left-right;break;
+//		case '*':answer=left*right;break;
+//		case '/':if(right!=0)answer=left/right;else display.setText("-1");return -1;
+//		}
+//		return answer;
+//    }
+//    public double calculateDouble(double left,double right,char op){
+//    	double answer=0;
+//		switch(op){
+//		case '+':answer=left+right;break;
+//		case '-':answer=left-right;break;
+//		case '*':answer=left*right;break;
+//		case '/':if(right!=0)answer=left/right;else display.setText("-1");return -1;
+//		}
+//		return answer;
+//    }
     private void handleOperator(String label) {
 		// TODO Auto-generated method stub
     	if(!label.equals("=")){
-    		left=Double.parseDouble(display.getText());
+    		if(isdouble){
+//    			if(isDouble(display.getText())){
+//					dleft=Double.parseDouble(display.getText());
+//					}
+//    			else{
+    				bdleft=new BigDecimal(display.getText());
+//    			}
+    		}
+    		else{
+//    			if(isLong(display.getText())){
+//					lleft=Long.parseLong(display.getText());
+//				}
+//    			else{
+    				bileft=new BigInteger(display.getText());
+//				}
+    		}
     		op=label.charAt(0);
-    		display.setText(label);
-    	}else{
-    		if(op==' '){
-    			display.setText(String.valueOf(left));
+    		display.setText("");
+    	}
+    	else{
+//    		long lanswer=0;
+//    		double danswer=0;
+    		if(isdouble){
+//    			if(isDouble(display.getText())){
+//					dright=Double.parseDouble(display.getText());
+//    			}else{
+    				bdright=new BigDecimal(display.getText());
+    				calculateBigDecimal(bdleft, bdright, op);
+    				return;
+////    			}
+//    			danswer=calculateDouble(dleft,dright,op);
+//				display.setText(String.valueOf(danswer));
     		}else{
-    			double answer=0;
-    			right=Double.parseDouble(display.getText());
-    			switch(op){
-    			case '+':answer=left+right;break;
-    			case '-':answer=left-right;break;
-    			case '*':answer=left*right;break;
-    			case '/':if(right!=0)answer=left/right;else display.setText("ERROR");break;
-    			}
-    			display.setText(String.valueOf(answer));
+//    			if(isLong(display.getText())){
+//					lright=Long.parseLong(display.getText());
+//    			}else{
+    				biright=new BigInteger(display.getText());
+    				calculateBigInteger(bileft, biright, op);
+    				return;
+//			}
+//    			lanswer=calculateLong(lleft,lright,op);
+//				display.setText(String.valueOf(lanswer));
     		}
     	}
 	}
 
 	private void handleNumber(String label) {
 		// TODO Auto-generated method stub
-        double now;
-        if(display.getText().indexOf(".")>0){
-    	    now=Double.parseDouble(display.getText());
-        }
-        else {now=0;}
-    	double chosen=Double.parseDouble(label);
-		if(chosen!=0){
-			display.setText(String.valueOf(chosen+now*10));
+		if(label.equals(".")){
+			if(display.getText().indexOf('.')<0&&!display.getText().equals("")){
+			isdouble=true;
+			display.setText(display.getText()+label);
+			}
+			else
+			return;
 		}
+		else if(!display.getText().equals("0")){
+			display.setText(display.getText()+label);
+		}
+		else
+			display.setText(label);
 	}
 
 	private void handleReset() {
 		// TODO Auto-generated method stub
 		display.setText("0");
-		left=0;right=0;op=' ';
+//		dleft=0;dright=0;op=' ';
+//		lleft=0;lright=0;
+		op=' ';
+		bileft=null;
+		biright=null;
+		bdleft=null;
+		bdright=null;
 	}
 	// Is the first digit pressed?
     boolean isFirstDigit = true;
